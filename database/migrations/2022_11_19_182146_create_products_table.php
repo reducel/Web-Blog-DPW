3edaa21e8acccecda9c286a1c84dc66c427ae420
Fauzan Taqiyuddin <19102103@ittelkom-pwt.ac.id>
2022-11-24 20:52:42 +0700

    model product

--- database/migrations/2022_11_19_182146_create_products_table.php
+++ database/migrations/2022_11_19_182146_create_products_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('harga');
+            $table->string('image');
+            $table->enum('status', ['publish', 'draft']);
+            $table->text('desc');
             $table->timestamps();
         });
     }
